0df41fe2ccebecd66cd94aeac2839bb634cfe733
AnalyceSanabria <148904513+AnalyceSanabria@users.noreply.github.com>
2023-10-24 20:01:20 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,44 @@
+function tocaSomPom (){
+    document.querySelector('.tecla_pom');
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap(){
+    document.querySelector('.tecla_clap');
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function tocaSomTim(){
+    document.querySelector('.tecla_tim');
+    document.querySelector('#som_tecla_tim').play();
+}
+
+function tocaSomPuff(){
+    document.querySelector('.tecla_puff');
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function tocaSomSplash(){
+    document.querySelector('.tecla_Splash');
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function tocaSomToim(){
+    document.querySelector('.tecla_toim');
+    document.querySelector('#som_tecla_toim').play();
+}
+
+function tocaSomPsh(){
+    document.querySelector('.tecla_psh');
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function tocaSomTic(){
+    document.querySelector('.tecla_tic');
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function tocaSomTom(){
+    document.querySelector('.tecla_tom');
+    document.querySelector('#som_tecla_tom').play();
+}
